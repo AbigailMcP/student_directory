f6c45db9c3e0b704e8e9f602e27386b6c3c9e534
Abigail <abigail.mcphillips@gmail.com>
2016-08-01 13:50:04 +0100

    Print only students names starting with specific letter

--- directory.rb
+++ directory.rb
@@ -23,7 +23,11 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index {|student, index| puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"}
+  students.each_with_index do |student, index|
+    if student[:name][0] == "A"
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
 end
 
 def print_footer(students)
