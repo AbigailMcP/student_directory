99c7886614873f7ab5288209015968dfca3ea0f6
Abigail <abigail.mcphillips@gmail.com>
2016-08-03 14:49:43 +0100

    Load saved student info on startup

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -42,17 +42,17 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     puts "What cohort are they in?"
-    cohort = gets.chomp.capitalize.to_sym
+    cohort = STDIN.gets.chomp.capitalize.to_sym
     if cohort.empty? == true then cohort = :November end
     # add the student hash to the array
     @students << {name: name, cohort: cohort}
     puts @students.count == 1 ? "Now we have #{@students.count} student" : "Now we have #{@students.count} students"
     # get anther name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -74,6 +74,7 @@ end
 
 def print_footer
   puts @students.count == 1 ? "Overall, we have #{@students.count} great student" : "Overall, we have #{@students.count} great students"
+  puts
 end
 
 def save_students
@@ -88,8 +89,20 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exists
+    puts "Sorry, #{filename} doesn't exist"
+    exit # quit the program
+  end
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -97,4 +110,5 @@ def load_students
   file.close
 end
 
+try_load_students
 interactive_menu
