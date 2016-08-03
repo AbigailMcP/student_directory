397a7f9d0c9989e8d1768423dc4aae1e0277c92b
Abigail <abigail.mcphillips@gmail.com>
2016-08-03 12:44:58 +0100

    Save student list to a file

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -19,6 +20,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -42,14 +45,8 @@ def input_students
     puts "What cohort are they in?"
     cohort = gets.chomp.capitalize.to_sym
     if cohort.empty? == true then cohort = :November end
-    puts "What country were they born in?"
-    country = gets.chomp
-    puts "What is their favourite hobby?"
-    hobby = gets.chomp
-    puts "How tall are they?"
-    height = gets.chomp
     # add the student hash to the array
-    @students << {name: name, cohort: cohort, country: country, hobby: hobby, height: height}
+    @students << {name: name, cohort: cohort}
     puts @students.count == 1 ? "Now we have #{@students.count} student" : "Now we have #{@students.count} students"
     # get anther name from the user
     name = gets.chomp
@@ -57,30 +54,35 @@ def input_students
 end
 
 def print_header
-  if !@students.empty?
   puts "The students of Villains Academy"
   puts "-------------"
-  end
 end
 
 def print_students_list
-  if !@students.empty?
     cohorts = @students.map {|student| student[:cohort]}.uniq
     cohorts.each do |cohort|
       @students.each do |student|
         if student[:cohort] == cohort
           puts "#{student[:name]} (#{student[:cohort].capitalize} cohort)"
-          puts "Country: #{student[:country]}"
-          puts "Hobby: #{student[:hobby]}"
-          puts "Height: #{student[:height]}\n\n"
         end
       end
     end
-  end
 end
 
 def print_footer
   puts @students.count == 1 ? "Overall, we have #{@students.count} great student" : "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
