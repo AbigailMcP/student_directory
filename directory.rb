fa795efed25a3f12223f3e7e4d21369832489660
Abigail <abigail.mcphillips@gmail.com>
2016-08-03 12:13:08 +0100

    Include students array as instance variable

--- directory.rb
+++ directory.rb
@@ -1,20 +1,14 @@
+@students = [] # an empty array accessible to all methods
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
+    print_menu
     selection = gets.chomp
-    # 3. do what the user has asked
     case selection
       when "1"
-        students = input_students
+        @students = input_students
       when "2"
-        print_header(students)
-        print(students)
-        print_footer(students)
+        show_students
       when "9"
         exit
       else
@@ -23,11 +17,21 @@ def interactive_menu
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print
+  print_footer
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -42,27 +46,27 @@ def input_students
     puts "How tall are they?"
     height = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: cohort, country: country, hobby: hobby, height: height}
-    puts students.count == 1 ? "Now we have #{students.count} student" : "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort, country: country, hobby: hobby, height: height}
+    puts @students.count == 1 ? "Now we have #{@students.count} student" : "Now we have #{@students.count} students"
     # get anther name from the user
     name = gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
 
-def print_header(students)
-  if !students.empty?
+def print_header
+  if !@students.empty?
   puts "The students of Villains Academy"
   puts "-------------"
   end
 end
 
-def print(students)
-  if !students.empty?
-    cohorts = students.map {|student| student[:cohort]}.uniq
+def print
+  if !@students.empty?
+    cohorts = @students.map {|student| student[:cohort]}.uniq
     cohorts.each do |cohort|
-      students.each do |student|
+      @students.each do |student|
         if student[:cohort] == cohort
           puts "#{student[:name]} (#{student[:cohort].capitalize} cohort)"
           puts "Country: #{student[:country]}"
@@ -74,8 +78,8 @@ def print(students)
   end
 end
 
-def print_footer(students)
-  puts students.count == 1 ? "Overall, we have #{students.count} great student" : "Overall, we have #{students.count} great students"
+def print_footer
+  puts @students.count == 1 ? "Overall, we have #{@students.count} great student" : "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
